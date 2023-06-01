67a13deffb3b9cb80072f40939a5f6b4e8ad36dc
giddyd <67126271+giddyd@users.noreply.github.com>
2023-06-01 15:55:02 -0400

    Update fabricinv.py

--- fabricinv.py
+++ fabricinv.py
@@ -1,7 +1,7 @@
 from dash import Dash
 import plotly.express as px
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import as dcc
+from dash import html
 from dash.dependencies import Input, Output, State
 
 import pandas as pd
